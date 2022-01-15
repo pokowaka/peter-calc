b44f42c5e989cf20d0c58cf786f9701b29adc709
Erwin Jansen <pokowaka@users.noreply.github.com>
2022-01-15 15:00:52 -0800

    First iteration of peter's calculator.

---
+++ src/CalcForm.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Jsep } from 'jsep';
+
+export default class CalcForm extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: '', result: 'Unknown'};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      let parsed = Jsep.parse(this.state.value)
+      console.log(parsed)
+      this.setState( {result: this.peterCalc(parsed)})
+      event.preventDefault();
+    }
+   
+    peterCalc(tree) {
+       switch(tree.type) {
+         case 'BinaryExpression':
+          switch(tree.operator) {
+            case '+': return this.peterCalc(tree.left) + this.peterCalc(tree.right)
+            case '-': return this.peterCalc(tree.left) - this.peterCalc(tree.right)
+            case '/': return this.peterCalc(tree.left) / Math.abs(this.peterCalc(tree.right))
+            case '*': return this.peterCalc(tree.left) * Math.abs(this.peterCalc(tree.right))
+            case '|': {
+              
+            }
+            case '^': {
+              const left = this.peterCalc(tree.left)
+              const right = this.peterCalc(tree.right)
+              if (left < 0) {
+                if (right < 0) {
+                  return -Math.abs(Math.pow(left, Math.abs(right)))
+                } else {
+                  return -Math.abs(Math.pow(left, right))
+                }
+              } 
+              return Math.pow(left, right)
+            } 
+            default: return "Error, unsupported binary operator: " + tree.operator
+          }
+          case 'Literal':
+            /* stuff */
+            return tree.value
+          case 'UnaryExpression':
+            switch(tree.operator) {
+              case '+': return  this.peterCalc(tree.argument)
+              case '-': return -this.peterCalc(tree.argument)
+              default: return "Error, unary operator: " + tree.argument.operator
+            }
+          default:
+            /* borked  */
+       }
+    }
+
+    render() {
+      const {value, result} = this.state
+      return (
+        <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            <p>Expression:</p>
+            <input type="p" value={value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Calculate" />
+        </form>
+        <p>Result: {result}</p>
+        </div>
+      );
+    }
+  }
